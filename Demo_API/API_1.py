01d7f1c70c7b7749b4097ee21666f9ff126d1dbd
amithuu <amithkulkarni99@gmail.com>
2023-07-11 18:52:55 +0530

    arc: learned how to build api.s and some methods on get and posy as well.

---
+++ Demo_API/API_1.py
@@ -0,0 +1,42 @@
+from flask import *
+import json, time
+
+app = Flask(__name__)    # this is the name of the project "__name__"
+
+# Methods: GET/POST/PUT/DELETE
+# GET: it is used to request data from a specified resource.  [Retrieve]
+# POST : to create or pass any data.                          [Create]
+# PUT: it is used to update the existing data.                [Update]
+# delete : it is used to delete from the data.                [Delete]
+
+
+@app.route('/', methods=['GET'])
+def home_page():
+    data_set = {'Page': 'Home', 'Message': 'Successfully created an api', 'Time': time.time()}
+    json_dump = json.dumps(data_set)
+
+    return json_dump
+
+
+@app.route('/user/', methods=['GET'])
+def request_page():
+    user_request = str(request.args.get('user'))  # the query gets generated is now-. /user/?user= amith kulkarni
+
+    data_set = {'Page': 'Request', 'Message': f'We got the user name = {user_request}', 'Time': time.time()}
+    json_dump = json.dumps(data_set)
+
+    return json_dump
+
+
+if __name__ == '__main__':
+    # here we are assigning the name of the project as '__main__'  , its like this is the main server..
+    app.run(port=7777)  # to run the application on the local host, we use the port 7777, 7776, 7775 and so on...
+
+
+
+
+
+
+
+
+
---
+++ Demo_API/API_2.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# example for get request..
+
+
+@app.route("/get-user/<user_name>")  # <user_name> what ever the variable ritter inside the < > is the requested variable, path
+def get_user(user_name):
+    data_set = {"user_name": user_name, "message": " Hi i got the user user name i am learning get method in api", "name": "amith", "email": "amittalentplace@gmail.com"}
+    get_data = request.args.get("extra")
+
+    if get_data:
+        data_set["extra"] = get_data
+
+    return jsonify(data_set), 200   # here the 200 is status code for success. we have many like that.
+
+
+# example for post request:
+# to work on the post we need the data-base.. so we are just learning how it looks like.
+
+@app.route('/create-user', methods=["POST"])
+def create_user():
+    data = request.get_json()
+
+    return jsonify(data), 201
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=7777)
+
+
+
